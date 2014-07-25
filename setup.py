4b98b4c050c6403b5f89699a961f8a442e7595d7
Raj <kraj.reddy@lnmiit.ac.in>
2014-07-25 18:42:37 +0530

    Change to cpp file (since most of my demo will be on cpp)

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ try:
     os.mkdir("build")
 except OSError:
     pass
-module_fname = os.path.join("build", "my-module-binding.c")
+module_fname = os.path.join("build", "my-module-binding.cpp")
 with open(module_fname, "wt") as file_:
     print("Generating file {}".format(module_fname))
     generate(file_)
 
 mymodule = Extension('mymodule',
-                     sources = [module_fname, 'my-module.c'],
+                     sources = [module_fname, 'my-module.cpp'],
                      include_dirs=['.'])
 
 setup(name='PyBindGen-example',
